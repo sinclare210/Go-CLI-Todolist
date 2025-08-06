7413bf6b1f8e42397339ddbfce418203058ef11b
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-06 20:13:20 +0100

    feature(reccurence):add for loop

--- main.go
+++ main.go
@@ -5,26 +5,49 @@ import (
 )
 
 func main() {
+	var todo string
+	var option int
+	cont := "y"
+	var todos []string
 
-	shortGolang := "Watch Go crash course"
-	fullGolang := "Watch Nana Golang Full Course"
-	rewardDesert := "Reward mysefl ith a donut"
+	fmt.Println("##### Hello There! What's your plan for today #####")
+	fmt.Println()
+	for cont == "y" {
+		fmt.Println("{1 for Adding to todo, 2 for view all todo}")
+		fmt.Scanln(&option)
 
-	taskItems := []string{
-		shortGolang,
-		fullGolang,
-		rewardDesert,
+		switch option {
+		case 1:
+			fmt.Println("input todo")
+			fmt.Scanln(&todo)
+			todos = addTask(todos, todo)
+			viewTodos(todos)
+
+		case 2:
+			fmt.Println(todos)
+			viewTodos(todos)
+
+		default:
+			fmt.Println("Invalid Option, try again")
+
+		}
+		fmt.Print("Do you want to perform another opertaion")
+		fmt.Println()
+		fmt.Println("{y/n}")
+		fmt.Scanln(&cont)
+		
+
 	}
 
-	fmt.Println("##### Welcome to our Todolist App! ######")
-	fmt.Println()
-	fmt.Println("List of my Todos")
-	fmt.Println()
+}
 
-	for i,e := range taskItems {
-		fmt.Println(i+1,". ", e)
+func addTask(todos []string, todo string) []string {
+	todos = append(todos, todo)
+	return todos
+}
+
+func viewTodos(todos []string) {
+	for i, e := range todos {
+		fmt.Println(i+1, ".", e)
 	}
-
-	
-
 }
