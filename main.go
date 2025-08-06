29ec6afcd86ced270891f8fa14c1a054d455805d
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-06 09:57:36 +0100

    Starting my first todo in GO

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	fmt.Println("##### Welcome to our Todolist App! #####")
+	fmt.Println("1. Watch Go Crash Course")
+	fmt.Println("2. Watch Nana's Golang Full Course")
+	fmt.Println("3. Reward Myself With a Cheese Cake")
+}
